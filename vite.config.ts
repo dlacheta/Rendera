a15cba8fb3072be80ef21842ea8551a05ed27881
Dariusz Lacheta <dariusz.lacheta@b3group.pl>
2025-01-02 09:45:45 +0100

    Build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: "./src/main.tsx", // główny punkt wejścia Twojej paczki
+      name: "rendera", // nazwa Twojej biblioteki
+      fileName: (format) => `rendera.${format}.js`,
+    },
+    rollupOptions: {
+      // Upewnij się, że te zależności są traktowane jako zewnętrzne
+      external: ["react", "react-dom"],
+      output: [
+        {
+          format: "es", // Format ES Modules
+          entryFileNames: "rendera.es.js",
+        },
+        {
+          name: "rendera",
+          format: "umd", // Format UMD
+          entryFileNames: "rendera.umd.js",
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
+        },
+        {
+          format: "cjs", // Format CommonJS
+          entryFileNames: "rendera.cjs.js",
+        },
+      ],
+    },
+  },
+});
